081f49aa2e6c320f43a6e2a40d24952be694a7bc
shuvamc019 <shuvamc19@gmail.com>
2022-03-28 18:55:04 -0400

    new changes

--- p3_signals.jl
+++ p3_signals.jl
@@ -12,12 +12,7 @@ function find_envelope(x; h::Int = 1000) # sliding window half-width
     zeros(h)]
 end
 
-function find_frequency(attack; release)
-    x = song[attack:release]
-    X = (2/length(x)) * abs.(fft(x))
-end
-
-threshold = 0.05;
+threshold = 0.015;
 
 global envelope = find_envelope(song)
 #global durations = [zeros(3) for i in size(song)[1]]
@@ -28,21 +23,70 @@ function find_durations()
     note_release_time = -1
     note_val = -1
 
+    skip = -1
+
     for t in 1:length(envelope)
-        signal_val = envelope[t]
-        if signal_val > threshold && note_attack_time == -1
-            note_attack_time = t
-            note_val = signal_val
-        elseif signal_val < threshold && note_attack_time != -1
-            note_release_time = t 
-            push!(durations, [note_val, note_attack_time, note_release_time])
-            note_attack_time = -1
-            note_val = -1
+        if t >= skip
+            signal_val = envelope[t]
+            if signal_val > threshold && note_attack_time == -1
+                note_attack_time = t
+                note_val = signal_val
+            elseif signal_val < threshold && note_attack_time != -1
+                note_release_time = t 
+                push!(durations, [note_val, note_attack_time, note_release_time])
+                note_attack_time = -1
+                note_val = -1
+                skip = t + 3500
+            end
         end
     end
 end
 
+global beats = []
+global bpm = 100 # give the user the ability to input this later on
+bps = bpm / 60
+
+function calculate_beats()
+    for i in 1:length(durations)
+        note = durations[i]
+        val = note[1]
+        duration = (note[3] - note[2]) / S
+
+        note_beats = duration * bps
+        note_beats = round(Integer, note_beats * 4) / 4 #rounds to nearest 0.25
+
+        push!(beats, [val, note_beats])
+
+        if i != length(durations)
+
+            next_note = durations[i + 1]
+            rest_duration = (next_note[2] - note[3]) / S
+
+            rest_beats = rest_duration * bps
+            rest_beats = round(Integer, rest_beats * 4) / 4
+
+            push!(beats, [-1, rest_beats])
+        end
+    end
+end
+
+global song_len = 0
+function song_length()
+    first = durations[1][2]
+    last =  durations[length(durations)][3]
+    song_len = (last - first) / S
+    song_len *= bps
+    song_len = round(Integer, song_len * 4) / 4
+end
+
 #compute frequencies
 
-
-
+function compute_frequencies()
+    for i in durations
+        start_time = i[2]
+        end_time = i[3]
+        N_seg = end_time - start_time
+        signal = song[start_time:end_time]
+        Signal = 2/N_seg * abs.(fft(signal))
+    end
+end
